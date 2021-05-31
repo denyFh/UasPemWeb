e1456581fc8f0bdbd66e8cc382dc50782ca3f0cb
denyFh <72296448+denyFh@users.noreply.github.com>
2021-05-31 09:17:21 +0700

    Initial Web Base

---
+++ index.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>GreenWorld</title>
+
+    <!-- Font Awesome Icons -->
+    <link rel="stylesheet" href="./css/all.css">
+
+
+    <!-- Owl-Carousel -->
+    <link rel="stylesheet" href="./css/owl.carousel.min.css">
+    <link rel="stylesheet" href="./css/owl.theme.default.min.css">
+
+    <!-- AOS Library  -->
+    <link rel="stylesheet" href="./css/aos.css">
+
+    <!-- Custom Style   -->
+    <link rel="stylesheet" href="./css/stylesheet.css">
+
+</head>
+
+<body>
+    
+    <!-- Main Site Section -->
+
+    <main>
+
+        <!-- Site Title -->
+
+        <section class="site-title">
+            <nav class="nav">
+                <div class="nav-menu flex-row">
+                    <div class="nav-brand">
+                        <a href="#" class="text-gray">GreenWorld</a>
+                    </div>
+                </div>
+            </nav>
+            <div class="site-background" data-aos="fade-up" data-aos-delay="100">
+                <h3>Save the Earth</h3>
+                <h1>Protect Our Forest</h1>
+                <button class="btn">Explore</button>
+                <button class="btn-2">Join Now!</button>
+            </div>
+            
+        </section>
+
+        <!-- Site Title -->
+
+        <!-- Blog Carousel -->
+
+        <section class="carousel-sect">
+            <div class="blog">
+                <div class="container">
+                    <div class="owl-carousel owl-theme blog-post">
+                        <div class="blog-content" data-aos="fade-left" data-aos-delay="200">
+                            <img src="./assets/Forest-post/fore-1.jpg" alt="post-1">
+                            <div class="blog-title">
+                                <h3>Forest Endangered From Human Unresponsibility</h3>
+                                <button class="btn btn-blog">Read More</button>
+                            </div>
+                        </div>
+                        <div class="blog-content" data-aos="fade-left" data-aos-delay="200">
+                            <img src="./assets/Forest-post/fore-3.jpg" alt="post-1">
+                            <div class="blog-title">
+                                <h3>Take Action! Help Us Protect our Forest</h3>
+                                <button class="btn btn-blog">Read More</button>
+                            </div>
+                        </div>
+                        <div class="blog-content" data-aos="fade-left" data-aos-delay="200">
+                            <img src="./assets/Forest-post/fore-2.jpg" alt="post-1">
+                            <div class="blog-title">
+                                <h3>Forest Rebuilding Realization</h3>
+                                <button class="btn btn-blog">Read More</button>
+                            </div>
+                        </div>
+                        <div class="blog-content" data-aos="fade-left" data-aos-delay="200">
+                            <img src="./assets/Forest-post/fore-4.jpg" alt="post-1">
+                            <div class="blog-title">
+                                <h3>Make Forest as Fun Place to Go</h3>
+                                <button class="btn btn-blog">Read More</button>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="owl-navigation">
+                        <span class="owl-nav-prev"><i class="fas fa-long-arrow-alt-left"></i></span>
+                        <span class="owl-nav-next"><i class="fas fa-long-arrow-alt-right"></i></span>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <!-- Blog Carousel -->
+
+    </main>
+
+    <!-- Main Site Section -->
+
+    <!-- Jquery Library file -->
+    <script src="./js/Jquery3.4.1.min.js"></script>
+
+    <!-- Owl-Carousel js -->
+    <script src="./js/owl.carousel.min.js"></script>
+
+    <!-- AOS js Library -->
+    <script src="./js/aos.js"></script>
+
+    <!-- Custom Javascript file -->
+    <script src="./js/main.js"></script>
+</body>
+
+</html>
